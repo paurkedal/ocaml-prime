240147739a54f0a5e17473532f2d41d493678274
Petter Urkedal <paurkedal@gmail.com>
2013-10-01 22:36:07 +0200

    Document Prime_enumset and Prime_enummap.

--- src/prime_enummap.mli
+++ src/prime_enummap.mli
@@ -22,24 +22,67 @@ module type OrderedType = sig
 end
 
 module type S = sig
+
   type key
+  (** The type of the keys of the map. *)
+
   type 'a t
+  (** The type of the map. *)
 
   val empty : 'a t
+  (** The empty map. *)
+
   val singleton : key -> 'a -> 'a t
+  (** [singleton k e] is the one-element map binding [k] to [e]. *)
+
   val contains : key -> 'a t -> bool
+  (** [contains k m] is true iff [m] has a binding for [k]. *)
+
   val find : key -> 'a t -> 'a
+  (** [find k m] returns the binding for [k] in [m] or raises [Not_found] if
+      it is unbound. *)
+
   val locate : key -> 'a t -> int option
+  (** If [k] is bound in [m], then [locate k m] is [Some i] where [i] is the
+      number of other keys before [k] according to the key order, otherwise it
+      returns [None]. *)
+
   val get_binding : int -> 'a t -> key * 'a
+  (** [get_binding i m] is the [i]th binding of [m] according to the key
+      order. *)
+
   val min_binding : 'a t -> key * 'a
+  (** [min_binding m] is the binding of [m] with the smallest key. *)
+
   val max_binding : 'a t -> key * 'a
+  (** [max_binding m] is the binding of [m] with the largest key. *)
+
   val add : key -> 'a -> 'a t -> 'a t
+  (** [add k e m] is the map which agrees with [m] on all keys except that [k]
+      is mapped to [e]. *)
+
   val pop_min : 'a t -> key * 'a * 'a t
+  (** [pop_min m] is the tuple [(k, e, m')] where [(k, e)] is the binding of
+      [m] with the minimal key and [m'] is the remainder of [m]. *)
+
   val pop_max : 'a t -> key * 'a * 'a t
+  (** [pop_max m] is the tuple [(k, e, m')] where [(k, e)] is the binding of
+      [m] with the maximum key and [m'] is the remainder of [m]. *)
+
   val remove : key -> 'a t -> 'a t
+  (** [remove k m] is the map which agrees with [m] on all keys except that
+      [k] is unbound.  If [k] is unbound in [m], then [remove k m] is [m]. *)
+
   val card : 'a t -> int
+  (** [card m] is the cardinality of [m]. *)
+
   val fold : (key -> 'a -> 'b -> 'b) -> 'a t -> 'b -> 'b
+  (** [fold f m] is the composition of [f k e] for each [(k, e)] in [m],
+      applied in order of increasing keys. *)
+
   val iter : (key -> 'a -> unit) -> 'a t -> unit
+  (** [iter f m] calls [f k e] for each [(k, e)] in [m] in order of increasing
+      keys. *)
 end
 
 module Make (Key : OrderedType) : S with type key = Key.t
--- src/prime_enumset.mli
+++ src/prime_enumset.mli
@@ -22,23 +22,60 @@ module type OrderedType = sig
 end
 
 module type S = sig
+
   type elt
+  (** The type of the elements of the set. *)
+
   type t
+  (** The type of the set. *)
 
   val empty : t
+  (** The empty set. *)
+
   val singleton : elt -> t
+  (** [singleton e] is the set containing only [e]. *)
+
   val contains : elt -> t -> bool
+  (** [contains e s] is true iff [s] contains [e]. *)
+
   val locate : elt -> t -> int option
+  (** If [s] contains [e], then [locate e s] is [Some i] where [i] is the
+      number of elements in [s] which are smaller than [e], otherwise [locate
+      e s] is [None]. *)
+
   val get : int -> t -> elt
+  (** [get i s] is the [i]th element of [s] according to the element order. *)
+
   val min_elt : t -> elt
+  (** [min_elt s] is the smallest element of [s]. *)
+
   val max_elt : t -> elt
+  (** [max_elt s] is the largest element of [s]. *)
+
   val add : elt -> t -> t
+  (** [add e s] is the set containing [e] along with the elements of [s]. *)
+
   val remove : elt -> t -> t
+  (** [remove e s] is the set containing all elements of [s] except [e]. *)
+
   val pop_min : t -> elt * t
+  (** [pop_min s] is [(e, s')] where [e] is the smallest element of [s] and
+      [s'] contains the remaining elements. *)
+
   val pop_max : t -> elt * t
+  (** [pop_max s] is [(e, s')] where [e] is the largest element of [s] and
+      [s'] contains the remaining elements. *)
+
   val card : t -> int
+  (** [card s] is the cardinality of [s]. *)
+
   val fold : (elt -> 'a -> 'a) -> t -> 'a -> 'a
+  (** [fold f s] is the composition of [f e] for each [e] in [s] applied in
+      order of increasing [e]. *)
+
   val iter : (elt -> unit) -> t -> unit
+  (** [iter f s] calls [f e] for each [e] in [s] in order of increasing
+      elements. *)
 end
 
 module Make (Elt : OrderedType) : S with type elt = Elt.t
