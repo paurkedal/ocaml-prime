a34bf2566f0eaea28bac5f433093701c6f1887f1
Petter Urkedal <paurkedal@gmail.com>
2013-10-24 17:15:18 +0200

    Use OUnit asserts and s/include/open/ in string tests.

--- tests/test_string.ml
+++ tests/test_string.ml
@@ -14,48 +14,55 @@
  * along with this library.  If not, see <http://www.gnu.org/licenses/>.
  *)
 
-include Unprime_list
-include Unprime_char
-include Unprime_string
-include OUnit
+open Unprime
+open Unprime_char
+open Unprime_list
+open Unprime_string
+open OUnit
+
+let print_string_list xs =
+  "[" ^ String.concat "; " (List.map String.escaped xs) ^ "]"
+let assert_eq_int = OUnit.assert_equal ~printer:string_of_int
+let assert_eq_string = OUnit.assert_equal ~printer:String.escaped
+let assert_eq_string_list = OUnit.assert_equal ~printer:print_string_list
 
 let run () =
   let s = "I won't make bugs.  I won't make bugs.  I won't make bugs." in
   let s' = "IwontmakebugsIwontmakebugsIwontmakebugs" in
   let n = String.length s in
   let xs = String.to_list s in
-  assert (String.of_list xs = s);
-  assert (String.fold List.push s [] = List.rev xs);
+  assert_eq_string s (String.of_list xs);
+  assert_equal (List.rev xs) (String.fold List.push s []);
   assert (String.for_all Char.is_ascii s);
   assert (not (String.for_all Char.is_alpha s));
   assert (String.exists Char.is_punct s);
   assert (not (String.exists Char.is_digit s));
-  assert (String.filter Char.is_alpha s = s');
+  assert_equal (s') (String.filter Char.is_alpha s);
 
-  assert (String.skip_while Char.is_alpha s 8 = 12);
-  assert (String.skip_until Char.is_space s 8 = 12);
-  assert (String.rskip_while Char.is_alpha s 12 = 8);
-  assert (String.rskip_until Char.is_space s 12 = 8);
-  assert (String.skip_while Char.is_ascii s 0 = n);
-  assert (String.rskip_while Char.is_ascii s n = 0);
+  assert_eq_int 12 (String.skip_while Char.is_alpha s 8);
+  assert_eq_int 12 (String.skip_until Char.is_space s 8);
+  assert_eq_int 8 (String.rskip_while Char.is_alpha s 12);
+  assert_eq_int 8 (String.rskip_until Char.is_space s 12);
+  assert_eq_int n (String.skip_while Char.is_ascii s 0);
+  assert_eq_int 0 (String.rskip_while Char.is_ascii s n);
 
   for i = 0 to n do
-    assert (String.skip_affix "" s i = Some i);
-    assert (String.rskip_affix "" s i = Some i);
-    assert (String.skip_affix "$" s i = None)
+    assert_equal (Some i) (String.skip_affix "" s i);
+    assert_equal (Some i) (String.rskip_affix "" s i);
+    assert_equal None (String.skip_affix "$" s i)
   done;
-  assert (String.skip_affix "->" "->" 0 = Some 2);
-  assert (String.skip_affix "->" "a->b" 0 = Some 3);
-  assert (String.skip_affix "->" "a->b" 1 = Some 3);
-  assert (String.skip_affix "->" "a->b" 2 = None);
-  assert (String.rskip_affix "->" "->" 2 = Some 0);
-  assert (String.rskip_affix "->" "a->b" 4 = Some 1);
-  assert (String.rskip_affix "->" "a->b" 3 = Some 1);
-  assert (String.rskip_affix "->" "a->b" 2 = None);
+  assert_equal (Some 2) (String.skip_affix "->" "->" 0);
+  assert_equal (Some 3) (String.skip_affix "->" "a->b" 0);
+  assert_equal (Some 3) (String.skip_affix "->" "a->b" 1);
+  assert_equal None (String.skip_affix "->" "a->b" 2);
+  assert_equal (Some 0) (String.rskip_affix "->" "->" 2);
+  assert_equal (Some 1) (String.rskip_affix "->" "a->b" 4);
+  assert_equal (Some 1) (String.rskip_affix "->" "a->b" 3);
+  assert_equal None (String.rskip_affix "->" "a->b" 2);
 
-  assert (String.slice 8 8 s = "");
-  assert (String.slice 8 12 s = "make");
-  assert (String.slice (n - 5) n s = "bugs.");
+  assert_eq_string "" (String.slice 8 8 s);
+  assert_eq_string "make" (String.slice 8 12 s);
+  assert_eq_string "bugs." (String.slice (n - 5) n s);
   assert (String.has_prefix "" s);
   assert (String.has_prefix "I won't" s);
   assert (not (String.has_prefix "n" s));
@@ -64,21 +71,21 @@ let run () =
   assert (not (String.has_suffix "?" s));
   assert (String.has_slice 8 "make" s);
 
-  assert (String.cut_affix "" "a" = Some ("", "a"));
-  assert (String.cut_affix "->" "->" = Some ("", ""));
-  assert (String.cut_affix "->" "a->b->c" = Some ("a", "b->c"));
-  assert (String.cut_affix "--" "a-a" = None);
-  assert (String.rcut_affix "" "a" = Some ("a", ""));
-  assert (String.rcut_affix "->" "->" = Some ("", ""));
-  assert (String.rcut_affix "->" "a->b->c" = Some ("a->b", "c"));
-  assert (String.rcut_affix "--" "a-a" = None);
+  assert_equal (Some ("", "a")) (String.cut_affix "" "a");
+  assert_equal (Some ("", "")) (String.cut_affix "->" "->");
+  assert_equal (Some ("a", "b->c")) (String.cut_affix "->" "a->b->c");
+  assert_equal None (String.cut_affix "--" "a-a");
+  assert_equal (Some ("a", "")) (String.rcut_affix "" "a");
+  assert_equal (Some ("", "")) (String.rcut_affix "->" "->");
+  assert_equal (Some ("a->b", "c")) (String.rcut_affix "->" "a->b->c");
+  assert_equal None (String.rcut_affix "--" "a-a");
 
-  assert (String.chop_affix "->" "" = []);
-  assert (String.chop_affix "->" "a" = ["a"]);
-  assert (String.chop_affix "->" "->" = [""; ""]);
-  assert (String.chop_affix "->" "a->" = ["a"; ""]);
-  assert (String.chop_affix "->" "->b" = [""; "b"]);
-  assert (String.chop_affix "->" "a->b" = ["a"; "b"]);
-  assert (String.chop_affix "->" "a-->b-->c+" = ["a-"; "b-"; "c+"]);
+  assert_eq_string_list [] (String.chop_affix "->" "");
+  assert_eq_string_list ["a"] (String.chop_affix "->" "a");
+  assert_eq_string_list [""; ""] (String.chop_affix "->" "->");
+  assert_eq_string_list ["a"; ""] (String.chop_affix "->" "a->");
+  assert_eq_string_list [""; "b"] (String.chop_affix "->" "->b");
+  assert_eq_string_list ["a"; "b"] (String.chop_affix "->" "a->b");
+  assert_eq_string_list ["a-";"b-";"c+"] (String.chop_affix "->" "a-->b-->c+");
 
   ()
